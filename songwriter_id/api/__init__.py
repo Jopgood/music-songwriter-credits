7ecac137c3c0007b95d494e2d5b58340cdda1246
Jopgood <jehopgood@gmail.com>
2025-04-13 16:58:09 +0100

    Make AcoustID import optional in API module

--- songwriter_id/api/__init__.py
+++ songwriter_id/api/__init__.py
@@ -1,6 +1,10 @@
 """API integration package for the songwriter identification system."""
 
 from songwriter_id.api.musicbrainz import MusicBrainzClient
-from songwriter_id.api.acoustid import AcoustIDClient
 
-__all__ = ['MusicBrainzClient', 'AcoustIDClient']
+# Import AcoustIDClient only if the chromaprint library is available
+try:
+    from songwriter_id.api.acoustid import AcoustIDClient
+    __all__ = ['MusicBrainzClient', 'AcoustIDClient']
+except ImportError:
+    __all__ = ['MusicBrainzClient']
